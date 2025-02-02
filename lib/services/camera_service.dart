9c261b225a718fdb175b0b4dbd872b1aa8f47230
André Pedersen <andrped94@gmail.com>
2025-02-02 11:02:39 +0100

    Fix camera permissions; allow selecting photo instead of snap

--- lib/services/camera_service.dart
+++ lib/services/camera_service.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
+import 'dart:async';
+import 'package:image/image.dart' as img;
 import 'tflite_service.dart';
 
 class CameraScreen extends StatefulWidget {
@@ -12,11 +15,19 @@ class _CameraScreenState extends State<CameraScreen> {
   File? _image;
   List<Map<String, dynamic>>? _results;
   final TFLiteHelper _tfliteHelper = TFLiteHelper();
+  double? _imageWidth;
+  double? _imageHeight;
 
   @override
   void initState() {
     super.initState();
+    _requestPermissions();
     _tfliteHelper.loadModel();
+  }
+
+  Future<void> _requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.storage.request();
   }
 
   @override
@@ -25,13 +36,17 @@ class _CameraScreenState extends State<CameraScreen> {
     super.dispose();
   }
 
-  Future<void> _pickImage() async {
-    final pickedFile = await ImagePicker().getImage(source: ImageSource.camera);
+  Future<void> _pickImage({required ImageSource source}) async {
+    final pickedFile = await ImagePicker().getImage(source: source);
     if (pickedFile != null) {
+      final image = File(pickedFile.path);
+      final decodedImage = img.decodeImage(image.readAsBytesSync());
       setState(() {
-        _image = File(pickedFile.path);
+        _image = image;
+        _imageWidth = decodedImage?.width.toDouble();
+        _imageHeight = decodedImage?.height.toDouble();
       });
-      _classifyImage(File(pickedFile.path));
+      _classifyImage(image);
     }
   }
 
@@ -49,28 +64,37 @@ class _CameraScreenState extends State<CameraScreen> {
         title: Text('Snap Pokémon Card'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null
-                ? Text('No image selected.')
-                : Image.file(_image!),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Capture Image'),
-            ),
-            SizedBox(height: 20),
-            _results == null
-                ? Container()
-                : Column(
-                    children: _results!.map((result) {
-                      return Text(
-                        "${result['label']} - ${(result['confidence'] * 100).toStringAsFixed(2)}%",
-                      );
-                    }).toList(),
-                  ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _results == null
+                  ? Container()
+                  : Column(
+                      children: _results!.map((result) {
+                        return Text(
+                          "${result['label']} - ${(result['confidence'] * 100).toStringAsFixed(2)}%",
+                        );
+                      }).toList(),
+                    ),
+              SizedBox(height: 20),
+              _image == null
+                  ? Text('No image selected.')
+                  : AspectRatio(
+                      aspectRatio: _imageWidth! / _imageHeight!,
+                      child: Image.file(_image!),
+                    ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _pickImage(source: ImageSource.camera),
+                child: Text('Capture Image'),
+              ),
+              ElevatedButton(
+                onPressed: () => _pickImage(source: ImageSource.gallery),
+                child: Text('Select Image from Gallery'),
+              ),
+            ],
+          ),
         ),
       ),
     );
