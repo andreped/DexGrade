8fb72848ee38f64bb9ea16f185b409b8e6489364
André Pedersen <andrped94@gmail.com>
2025-02-02 11:27:58 +0100

    Improve prediction UI UX; add real-time inference support (#3)

--- lib/services/camera_service.dart
+++ lib/services/camera_service.dart
@@ -63,39 +63,119 @@ class _CameraScreenState extends State<CameraScreen> {
       appBar: AppBar(
         title: Text('Snap Pokémon Card'),
       ),
-      body: Center(
+      body: Align(
+        alignment: Alignment.topCenter,
         child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               _results == null
                   ? Container()
-                  : Column(
-                      children: _results!.map((result) {
-                        return Text(
-                          "${result['label']} - ${(result['confidence'] * 100).toStringAsFixed(2)}%",
-                        );
-                      }).toList(),
+                  : Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: _buildRecognitionList(),
                     ),
               SizedBox(height: 20),
               _image == null
                   ? Text('No image selected.')
-                  : AspectRatio(
-                      aspectRatio: _imageWidth! / _imageHeight!,
-                      child: Image.file(_image!),
-                    ),
+                  : _buildImage(),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => _pickImage(source: ImageSource.camera),
-                child: Text('Capture Image'),
-              ),
-              ElevatedButton(
-                onPressed: () => _pickImage(source: ImageSource.gallery),
-                child: Text('Select Image from Gallery'),
+              Center(
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _pickImage(source: ImageSource.camera),
+                      child: Text('Capture Image'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _pickImage(source: ImageSource.gallery),
+                      child: Text('Select Image from Gallery'),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    double maxHeight = MediaQuery.of(context).size.height * 0.5;
+    double maxWidth = MediaQuery.of(context).size.width * 0.9;
+
+    double imageAspectRatio = _imageWidth! / _imageHeight!;
+    double containerAspectRatio = maxWidth / maxHeight;
+
+    double displayWidth;
+    double displayHeight;
+
+    if (imageAspectRatio > containerAspectRatio) {
+      displayWidth = maxWidth;
+      displayHeight = maxWidth / imageAspectRatio;
+    } else {
+      displayHeight = maxHeight;
+      displayWidth = maxHeight * imageAspectRatio;
+    }
+
+    return Center(
+      child: Container(
+        width: displayWidth,
+        height: displayHeight,
+        child: Image.file(_image!),
+      ),
+    );
+  }
+
+  Widget _buildRecognitionList() {
+    var _width = MediaQuery.of(context).size.width;
+    var _padding = 20.0;
+    var _labelWidth = 200.0;
+    var _labelConfidence = 50.0;
+    var _barWidth = _width - _labelWidth - _labelConfidence - _padding * 2.0;
+
+    return Container(
+      height: 150,
+      child: ListView.builder(
+        itemCount: _results!.length,
+        itemBuilder: (context, index) {
+          if (_results!.length > index) {
+            return Container(
+              height: 40,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: _padding, right: _padding),
+                    width: _labelWidth,
+                    child: Text(
+                      _results![index]['label'],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Container(
+                    width: _barWidth,
+                    child: LinearProgressIndicator(
+                      backgroundColor: Colors.transparent,
+                      value: _results![index]['confidence'],
+                    ),
+                  ),
+                  Container(
+                    width: _labelConfidence,
+                    child: Text(
+                      (_results![index]['confidence'] * 100).toStringAsFixed(1) + '%',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
       ),
     );
   }
